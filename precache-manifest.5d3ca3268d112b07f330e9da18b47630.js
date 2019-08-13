1035e47089e2a49369d0a1e1f6dcd87a4daaff4d
chris13012 <christophecaillet@icloud.com>
2019-08-13 14:39:45 +0200

    Updates

---
+++ precache-manifest.5d3ca3268d112b07f330e9da18b47630.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ccb619e095d3b8b5aab303e2ae4c4f8",
+    "url": "/shoppingcart/index.html"
+  },
+  {
+    "revision": "1d3e8bffd8d29e66ebbc",
+    "url": "/shoppingcart/static/css/2.d35aef79.chunk.css"
+  },
+  {
+    "revision": "4c978d81da67bb5e9f00",
+    "url": "/shoppingcart/static/css/main.de976d04.chunk.css"
+  },
+  {
+    "revision": "1d3e8bffd8d29e66ebbc",
+    "url": "/shoppingcart/static/js/2.d959f4cb.chunk.js"
+  },
+  {
+    "revision": "4c978d81da67bb5e9f00",
+    "url": "/shoppingcart/static/js/main.9d4258ec.chunk.js"
+  },
+  {
+    "revision": "bf4b4c4b8004226b385d",
+    "url": "/shoppingcart/static/js/runtime~main.105bbe4f.js"
+  }
+]);
